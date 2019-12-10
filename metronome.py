d5ac9b36b97bb76568d3377c940e7afdb36d5f58
kevin <khynes57@outlook.com>
2019-12-10 15:54:07 -0500

    sloppy, but Metronome now able to stay consistent using numpy/scipy to make a wav file and ffplay/subprocess to play it

--- metronome.py
+++ metronome.py
@@ -4,7 +4,9 @@ from kivy.core.audio import SoundLoader
 from kivy.clock import Clock
 from threading import Thread, Event
 
-import time
+import numpy as np
+import time, scipy.io.wavfile as wavfile
+import subprocess
 
 
 class Metronome(FloatLayout):
@@ -15,8 +17,18 @@ class Metronome(FloatLayout):
         self.starttime = time.time()
         self.stopped = Event()
         self.thread = Thread(target=self.play, daemon=True)
-        self.interval = 0.25
-        self.thread.start()
+        self.interval = 0.75
+        # read_rate, read_data = wavfile.read("./sounds/Clap-1.wav")
+        read_data = [[1, 1] for channel_sample in range(10000)]
+        rest = [[0, 0] for channel_sample in read_data]
+        write_data = read_data + rest + read_data + rest + read_data + rest + read_data + rest
+        wav_write = wavfile.write("sound.wav", 44100, np.array(write_data, dtype="float32"))
+
+        self.sound = SoundLoader.load("sound.wav")
+        self.sound.loop = True
+        # self.sound.play()
+        # self.thread.start()
+        completed = subprocess.run(["ffplay/bin/ffplay.exe", "-loop", "0", "-nodisp", "sound.wav"])
 
     def play(self):
         while not self.stopped.wait(self.interval - (time.time() - self.starttime) % self.interval):
@@ -24,7 +36,7 @@ class Metronome(FloatLayout):
                 print("stopped")
                 self.click.stop()
             print(self.interval - (time.time() - self.starttime) % self.interval)  # should be 0
-            self.click.play()
+            self.sound.play()
 
     def stop(self):
         self.stopped.set()
