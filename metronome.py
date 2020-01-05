865bac010c7e87cd27dbdecbb5519243d88fa016
kevin <khynes57@outlook.com>
2020-01-05 11:40:32 -0500

    Now using kivy Animation instead of custom (much cleaner) and animating the needle, beatmarkers, and playing sound from the same Thread! Works!

--- metronome.py
+++ metronome.py
@@ -10,11 +10,6 @@ from threading import Thread, Event
 import time, wave, pyaudio, math
 
 
-
-
-
-
-
 class TitleBar(BoxLayout):
 
     def validate_text(self, text):
@@ -25,15 +20,10 @@ class TitleBar(BoxLayout):
             i += 1
         if bpm and 1 <= int(bpm) <= 300:
             bpm = int(bpm)
-            if bpm == self.metronome.bpm:
-                self.metronome.bpm += 1
-                self.metronome.bpm -= 1
-            else:
-                self.metronome.bpm = bpm
-        else:
-            # Bpm entered was invalid.  Just change it & change it back to update the text.
-            self.metronome.bpm += 1
-            self.metronome.bpm -= 1
+            self.metronome.bpm = bpm
+        # In case bpm was invalid, out of range, or same value as before, make sure text is updated.
+        self.metronome.bpm += 1
+        self.metronome.bpm -= 1
 
 
 class BeatMarker(InstructionGroup):
@@ -76,42 +66,19 @@ class BeatMarker(InstructionGroup):
         self.anim_circle.size = size
         self.marker.size = size
 
-    def animate(self, parent, dur):
-        spb = parent.spb
-        self._initiate_animation(spb, dur)
-        # Binding to the animation.on_progress event only firing twice?  Going custom instead..
-        # animation = Animation(duration=dur, s=0)
-        # animation.bind(on_progress=print)
-        # animation.bind(on_complete=self._end_animation)
-        # animation.start(parent)
+    def update_animation(self, animation, metronome, progress):
+        rdiff = progress * self.max_rdiff
+        cx, cy = self.marker.pos
+        self.anim_circle.pos = cx - rdiff, cy - rdiff
+        self.anim_circle.size = [2 * (self.r + rdiff), 2 * (self.r + rdiff)]
+        self.anim_color.a = progress
 
-    def _initiate_animation(self, spb, duration):
-        self.anim_color.a = 1
-        self.anim_duration = duration
-        self.anim_start = time.time()
-        self._update_animation(spb)
-
-    def _update_animation(self, spb, *args):
-        while not self.stop_event.is_set():
-            progress = (time.time() - self.anim_start) / self.anim_duration
-            if progress > 1:
-                self.stop_event.set()
-                progress = 0
-            rdiff = progress * self.max_rdiff
-            cx, cy = self.marker.pos
-            self.anim_circle.pos = cx - rdiff, cy - rdiff
-            self.anim_circle.size = [2 * (self.r + rdiff), 2 * (self.r + rdiff)]
-            self.anim_color.a = progress
-            self.stop_event.wait(spb/100000)  # stops markers from freezing on hover?
-        self._end_animation()
-
-    def _end_animation(self, *args):
+    def end_animation(self, animation, metronome):
         self.anim_color.a = 0
         cx, cy = self.marker.pos
         d, d = self.marker.size
         self.anim_circle.pos = cx, cy
         self.anim_circle.size = d, d
-        self.stop_event.clear()
 
 
 class BeatBar(FloatLayout):
@@ -221,59 +188,15 @@ class Metronome(FloatLayout):
             rate=sine.getframerate(),
             output=True)
 
-        self.is_active = False
+        self.stopped = True
+        self.needle_animation = Animation()
+        self.beatmarker_animation = Animation()
 
     def play(self, *args):
-        if not self.is_active:
-            self.is_active = True
+        if self.stopped:
+            self.stopped = False
             thread = Thread(target=self._play, daemon=True)
             thread.start()
-
-    def _play(self, *args):
-        '''Update the Metronome's needle angle on every iteration, play beat at appropriate times.
-
-        Since progress goes from 0-1, and beats are being represented by max & min of cos wave,
-        we are constantly traversing 0-pi in the wave which would only move the needle from left
-        to right.  Keep track of beat_parity so we know if we should go right to left.
-        '''
-        beat_num = 0
-        beatmarker = self.beatbar.beatmarkers.children[0]
-        beat_parity = 1
-        start = time.time()
-        while not self.stop_event.is_set():
-            beats_so_far, t_after_b = divmod(time.time() - start, self.spb)
-            progress = t_after_b / self.spb
-            if beats_so_far > beat_num:
-                beat_num = int(beats_so_far)
-                beat_parity *= -1
-                # [1] Use wave and pyaudio modules to play chunk of real metronome .wav file.
-                # Works, but 1) imperfect timing due to using a real sound sample and 2) sound
-                # played is somehow not the same everytime - tends to change slightly.
-                # self.stream.write(self.accent_data)
-
-                # [2] Use simpleaudio to play NumPy array directly.
-                # Works, but prevents metronome needle from moving smoothly regardless of using wait_done().
-                # Works well with duration_s = 0.01 above.
-                # play_obj = sa.play_buffer(waveform_integers, 1, 2, sps)
-                # play_obj.wait_done()
-
-                # [3] Use SciPy to write NumPy array to .wav file, wave to open and read, pyaudio to play.
-                # Does not work correctly at all.  Changes to duration_s above affect it.
-                self.stream.write(self.sine_data)
-
-                beatmarker.animate(self, 0.1)
-                beatmarker = self.beatbar.beatmarkers.children[beat_num % self.num_beats]
-            self.needle_angle = self.max_needle_angle * math.cos(progress * math.pi) * beat_parity
-            self.stop_event.wait(self.spb/10000)  # prevents mouse hover from freezing needle (?)
-        self.stop_event.clear()
-
-    def stop(self, *args):
-        if self.is_active:
-            self.is_active = False
-            self.needle_angle = 0
-            self.stop_event.set()
-            for beatmarker in self.beatbar.beatmarkers.children:
-                beatmarker.stop_event.set()
 
     def close(self, *args):
         self.stream.close()
@@ -290,12 +213,58 @@ class Metronome(FloatLayout):
             self.box.height = width / target_ratio
 
     def increment_bpm(self, val):
-        self.bpm += val
+        if 1 < self.bpm < 300:
+            self.bpm += val
 
     def on_bpm(self, instance, bpm):
         self.spb = 60 / self.bpm
-        # self.stop()
-        # self.play()
+        # was_active = False
+        # if not self.stopped:
+        #     was_active = True
+        #     self.stop()
+        # if was_active:
+        #     time.sleep(0.1)
+        #     self.play()
+
+    def _play(self):
+        start = time.time()
+        delta = self.spb
+        goal = start + delta
+        forward = True
+        while not self.stopped:
+            beat_num = (time.time() - start) // delta
+            self.animate_needle(self.spb, forward)
+            self.animate_beatmarker(0.1, beat_num)
+            self.stream.write(self.sine_data)
+            time.sleep(goal - time.time())
+            forward = not forward
+            goal += delta
+
+    def stop(self):
+        if not self.stopped:
+            self.beatmarker_animation.stop(self)
+            self.needle_animation.stop(self)
+            self.stopped = True
+            self.needle_angle = 0
+
+    def animate_needle(self, duration, forward):
+        self.needle_animation.stop(self)
+        if forward:
+            self.needle_angle = -self.max_needle_angle
+            self.needle_animation = Animation(needle_angle=self.max_needle_angle, duration=duration)
+        else:
+            self.needle_angle = self.max_needle_angle
+            self.needle_animation = Animation(needle_angle=-self.max_needle_angle, duration=duration)
+        self.needle_animation.start(self)
+
+    def animate_beatmarker(self, duration, beat_num):
+        self.beatmarker_animation.stop(self)
+        self.beatmarker_animation = Animation(duration=duration)
+        beat_idx = int(beat_num % self.num_beats)
+        beatmarker = self.beatbar.beatmarkers.children[beat_idx]
+        self.beatmarker_animation.bind(on_progress=beatmarker.update_animation)
+        self.beatmarker_animation.bind(on_complete=beatmarker.end_animation)
+        self.beatmarker_animation.start(self)
 
 
 class MetronomeApp(App):
